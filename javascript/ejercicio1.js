7d95be1ebe1edeea3da22c45aac65e283b4160ac
Ana López <anamuyamiga@gmail.com>
2023-10-25 10:17:21 +0200

    fjldjfklsdf

---
+++ javascript/ejercicio1.js
@@ -0,0 +1,26 @@
+const fruit  = prompt ("Enter the fruit name:")
+let type = "";
+switch (fruit){
+  case "pear":
+  case "apple":
+    type = "pome";
+    break;
+  case "peach":
+  case "coconut":
+    type = "drupe";
+    break;
+  case "strawberry":
+  case "blueberry":
+  case "blackberry":
+    trype = "berry";
+    break;
+  case "beans":
+  case "peas":
+    type = "legumes";
+    break;
+  default:
+    type = "other";
+    break;
+}
+
+console.log(`${fruit} is type of: ${type}`);
