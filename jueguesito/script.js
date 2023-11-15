46f140b58164b70a0d2aac05b22f72df52a700e0
Ana López <anamuyamiga@gmail.com>
2023-11-15 12:13:43 +0100

    movidas

--- jueguesito/script.js
+++ jueguesito/script.js
@@ -3,6 +3,7 @@ const secretNumber = Math.floor(Math.random()*101);
 
 //ponemos el contador en 1, ya que el programa empieza en el primer intento
 let contador = 1
+let intento = 0
 
 function checkGuess(e) {
   //guardamos la respuesta del usuario en una variable, en el campo guess-input
@@ -11,34 +12,40 @@ function checkGuess(e) {
   //Esto indica que el botón está en funcionamiento
   e.preventDefault();
   console.log('I am working');
-  //Aquí enviamos un mensaje al usuario dependiendo de su respuesta, y le indicamos el número de intentos que lleva.
-  if (respuesta == secretNumber) {
-    console.log(`Felicidades, has acertado. Número de intentos: ${contador}`)
-    alert(`Felicidades, has acertado. Número de intentos: ${contador}`)
+  if (contador > intento) {
+    console.log("Has perdido")
+    boton.onclick = function(e){alert("Has perdido")};
+  }
+  else {
+    //Aquí enviamos un mensaje al usuario dependiendo de su respuesta, y le indicamos el número de intentos que lleva.
+    if (respuesta == secretNumber) {
+      console.log(`Felicidades, has acertado. Número de intentos: ${contador}`)
+      alert(`Felicidades, has acertado. Número de intentos: ${contador}`)
+      
+    }
+    else if (respuesta > secretNumber) {
+      console.log(`Es demasiado alto. Número de intentos: ${contador}`)
+      alert(`Es demasiado alto. Número de intentos: ${contador}`)
+      contador++
+    }
     
-  }
-  else if (respuesta > secretNumber) {
-    console.log(`Es demasiado alto. Número de intentos: ${contador}`)
-    alert(`Es demasiado alto. Número de intentos: ${contador}`)
-    contador++
-  }
-  
-  else if (respuesta < secretNumber) {
-    console.log(`Es demasiado bajo. Número de intentos: ${contador}`)
-    alert(`Es demasiado bajo. Número de intentos: ${contador}`)
-    contador++
+    else if (respuesta < secretNumber) {
+      console.log(`Es demasiado bajo. Número de intentos: ${contador}`)
+      alert(`Es demasiado bajo. Número de intentos: ${contador}`)
+      contador++
+    }
   }
 }
 function dificultad() {
   let dificultad = prompt("Elige la dificultad: (F)fácil, (M)medio o (D)difícil");
   if (dificultad == "F") {
-    let intento = 20;
+    intento = 20;
   }
   else if (dificultad == "M") {
-    let intento = 10;
+    intento = 10;
   }
   else if (dificultad == "D") {
-    let intento = 5;
+    intento = 5;
   }
   else {
     alert("No has elegido una dificultad válida");
@@ -46,8 +53,11 @@ function dificultad() {
   return intento;
 }
 
+
+
 //Aquí relacionamos el botón de la web con la variable boton que vamos a utilizar
 let boton = document.getElementById("guess-btn");
+intento = dificultad();
 //indicamos que se active la función checkGuess cuando se cliqué el boton
 boton.onclick = function(e){checkGuess(e)};
 
